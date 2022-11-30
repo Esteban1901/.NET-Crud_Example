61adbda17777362778ab8e55d9cb2958cef6cfe0
Esteban1901 <esteban.reyesl@usm.cl>
2022-11-30 16:53:06 -0300

    Primer commit

---
+++ Main/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BLL;
+
+namespace Main
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CategoriaBLL catBll = new CategoriaBLL();
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListarCategorias();
+        }
+
+        private void ListarCategorias()
+        {
+            LstTareas.ItemsSource = catBll.GetAll();
+        }
+
+        private void BtnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                string nombre = TxtNombre.Text.Trim();
+                catBll.Add(nombre);
+
+                MessageBox.Show("Categoría agregada", "Nueva Categoría", MessageBoxButton.OK, MessageBoxImage.Information);
+                TxtNombre.Text = string.Empty;
+                ListarCategorias();
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Nueva Categoría", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void BtnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            string nombre = LstTareas.SelectedItem.ToString();
+
+            catBll.Delete(nombre);
+
+            ListarCategorias();
+        }
+    }
+}
---
+++ Negocio/CategoriaBLL.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+
+namespace BLL
+{
+    public class CategoriaBLL
+    {
+        TareasPendientesEntities tp = new TareasPendientesEntities();
+
+        public void Add(string nombre)
+        {
+
+            if (Get(nombre) == null)
+            {
+                Categoría nueva = new Categoría();
+                nueva.Nombre = nombre;
+
+                tp.Categoría.Add(nueva);
+                tp.SaveChanges();
+            }
+            else
+            {
+                throw new ArgumentException("Categoría ya existe");
+            }
+
+            
+        }
+
+        public Categoría Get(string nombre)
+        {
+            Categoría categoria = tp.Categoría.Where(c => c.Nombre == nombre).FirstOrDefault();
+
+            return categoria;
+        }
+
+        public List<Categoría> GetAll()
+        {
+            return tp.Categoría.ToList();
+        }
+
+        public void Delete(string nombre)
+        {
+            Categoría c = Get(nombre);
+
+            if (c != null)
+            {
+                tp.Categoría.Remove(c);
+                tp.SaveChanges();
+            }
+        }
+    }
+}
